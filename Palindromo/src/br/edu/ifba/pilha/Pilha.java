2ff9b54be98471b6bbba02591aee75bde0bc0947
201810070006 <201810070006@ifbapa003415.ifbapa.intra>
2022-11-08 12:07:23 -0300

    Commit alteração classe pilha

--- Palindromo/src/br/edu/ifba/pilha/Pilha.java
+++ Palindromo/src/br/edu/ifba/pilha/Pilha.java
@@ -24,7 +24,7 @@ public class Pilha {
 		if(palavra.equals(verifica.toString())) {
 			System.out.println("Ã‰ palindromo");
 		}else {
-			System.out.println("NÃ£o Ã©!");
+			System.out.println("Não é");
 		}
 	}
 	
